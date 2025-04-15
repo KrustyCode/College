c264f6ff1d23c7946cccfc575a4091443a722094
Red Coat <bayubujanks@gmail.com>
2025-04-15 16:38:35 +0800

    Init

---
+++ Pemrograman Berbasis Web/Tugas Web 3/buatevent.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>kelompok 3 :
+        <br>1. Bayu Fadjar Dwi Putra 2308561080
+        <br>2. Jovianka Pratama Santoso 2308561062
+        <br>3. Bagus Ajie Satria 2308561044
+        <br>4. Gede Nicholas Tejasukmana Putra 2308561074
+    </p>
+    <p>Nama Kegiatan: <?= $_POST['name'] ?></p>
+    <p>Tanggal Kegiatan: <?= $_POST['tgl_kegiatan'] ?></p>
+    <p>Tingkat Kegiatan: <?= $_POST['tingkat_kegiatan'] ?></p>
+    <p>Kuota Peserta: <?= $_POST['kuota'] ?></p>
+    <p>Quote of the day: <?= $_POST['sembunyi'] ?></p>
+</body>
+</html>
